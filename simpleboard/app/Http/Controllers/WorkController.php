6387744c2131864422f8db76d3777ae7b122724d
ironmaple0 <89458563+ironmaple0@users.noreply.github.com>
2021-10-14 17:59:42 +0900

    fifth

--- simpleboard/app/Http/Controllers/WorkController.php
+++ simpleboard/app/Http/Controllers/WorkController.php
@@ -8,11 +8,17 @@ class WorkController extends Controller
 {
     public function work(Request $request) {
         $data = $request-> input('textbox');
-            if($data >= 0 and $data < 50) {
+          if(is_numeric($data)){
+             if($data >= 0 and $data < 50) {
                $message = '頑張りましょう';
             }else if($data >=50 and $data < 80){
                 $message = 'よくできました';
-        }
+            }else if($data >=80 and $data <=100) {
+                $message = '大変よくできました';
+            }
+         }else{
+               $message = '数字を入力してください';
+         }
         //return view('testform')->with('message1', $message); 
         return view('testform',compact('message'));
    }
@@ -20,19 +26,27 @@ class WorkController extends Controller
     public function homework(Request $request) {
         $data2 = $request-> input('textbox');
         $num = 0;
-        for($i = 1; $i <=$data2; $i++){
-        $num= $num + $i;
-     }
+        if(is_numeric($data2)) {
+         for($i = 1; $i <=$data2; $i++){
+          $num= $num + $i;
+        }
+       }else {
+        $num = '数字を入力してください';
+       }
         return view('testform2',compact('num','data2'));
     } 
 
     public function assignment(Request $request) {
         $id = ["100","101","102","103","104","105"];
         $data3 = $request -> input('textbox');
-        if(in_array($data3,$id)){
+        if(is_numeric($data3)) {
+          if(in_array($data3,$id)){
             $memo = '登録されています';
-        }else{
+         }else{
             $memo = '登録されていません';
+         }
+       }else{
+            $memo = '数字を入力してください';
         }
          return view('testform3',compact('memo','data3'));
     }
